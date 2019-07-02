051ed019580c852500b7d96685cca2f86ee87552
jakob-huber <huber.jak+github@gmail.com>
2019-07-02 21:49:04 +0200

    commit

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -1,7 +1,17 @@
 <meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<meta name="description" content="">
+<meta name="author" content="">
+
 
 <title>samsa</title>
 
 <!-- verknüpfung zu Stylesheet  -->
 <link rel="stylesheet" href="{{ mix('/css/app.css')}}">
+
+<!-- Bootstrap core CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUIyJ" crossorigin="anonymous">
+
+
+
