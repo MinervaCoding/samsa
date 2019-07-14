5ffe119aae3489762ade5710fa49b6c3d636d659
jakob-huber <huber.jak+github@gmail.com>
2019-07-14 12:07:57 +0200

    Fixed Navigation Bar in Mobile Modus

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -2,6 +2,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <meta name="description" content="">
 <meta name="author" content="">
+
 <title>samsa</title>
 
 <!-- Bootstrap core CSS -->
--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -3,8 +3,15 @@
 
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
 
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="{{ __('Toggle navigation') }}">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
 
+            <!-- Left Side Of Navbar -->
             <ul class="navbar-nav mr-auto">
 
                 <li class="nav-item active">
@@ -43,20 +50,6 @@
 
                 @endguest
 
-            </ul>
-
-        </div>
-
-
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent" aria-expanded="false"
-                aria-label="{{ __('Toggle navigation') }}">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <!-- Left Side Of Navbar -->
-            <ul class="navbar-nav mr-auto">
 
             </ul>
 
@@ -112,8 +105,8 @@
                     </li>
                 @endguest
             </ul>
-        </div>
 
+        </div>
 
     </nav>
 
